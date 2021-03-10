b22fb89cfbcc6d4126bf892ff8a8ec7e62ae44ab
AnnaKhalikova <hanna.Khalikova@gmail.com>
2021-03-10 22:40:57 +0300

    rewrite functionality

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 function calculateFunction(a, n, c_a, c_t){
-    console.log("Function that calculate expression")
+    console.log("a = " + a + " n = " + n);
     return 1 / (a + (1 - a) / n + c_a * c_t)
 }
-function fun1() {
-    console.log("Function that checks radiobuttons")
-    var rad = document.getElementsByName('criteria');
-    for (var i = 0; i < rad.length; i++) {
+function getCheckedRadiobutton() {
+    let rad = document.getElementsByName('criteria');
+    for (let i = 0; i < rad.length; i++) {
         if (rad[i].checked) {
-            console.log('Выбран '+ i + ' radiobutton');
             return rad[i];
         }
     }
     return rad[0];
 }
 function drawChart(labels, data){
-    console.log("in drawChart func")
-    let ctx = document.getElementById('myChart').getContext('2d');
-    console.log("labels:" + labels);
-    console.log("Data: " + data);
 
+    let ctx = document.getElementById('myChart').getContext('2d');
+    //
     let chart = new Chart(ctx, {
     // Тип графика
     type: 'line',
@@ -32,73 +28,78 @@ function drawChart(labels, data){
             borderColor: 'rgb(255, 99, 132)', // Цвет линии
             data: data// Данные каждой точки графика
         }]
-    },
-    // Настройки графиков
-    options: {}
+    }
     });
 }
 function getDataSet(a, n, c_a, c_t, h, main_parameter){
-    var numbers = new Array();
+    let numbers = new Array();
     numbers = []
-    for(var i = 0; i <= main_parameter; i += h){
-        numbers.push(calculateFunction(i, n, c_a, c_t));
+    let i = 0;
+    if(main_parameter == n){
+        i = 1;
+    }
+    for(i; i <= main_parameter; i += h){
+        if(main_parameter === a){
+            //console.log("Actual main parameter number is: " + main_parameter);
+            numbers.push(calculateFunction(i, n, c_a, c_t).toFixed(3));
+        }
+        else if(main_parameter === n){
+            // console.log("Actual main parameter number is: " + main_parameter);
+            // console.log("a = " + a + " n = " + i)
+            numbers.push(calculateFunction(a, i, 0, 0).toFixed(3));
+        }
+        else if(main_parameter === c_a){
+            console.log("Actual main parameter number is: " + main_parameter);
+            numbers.push(calculateFunction(a, n, i, c_t).toFixed(3));
+        }
+        else if(main_parameter === c_t){
+            console.log("Actual main parameter number is: " + main_parameter);
+            numbers.push(calculateFunction(a, n, c_a, i).toFixed(3));
+        }
+        else{
+            console.log("Error. Actual main parameter number is: " + main_parameter);
+        }
     }
     return numbers;
 }
 function getLabelsSet(parameter, step){
-    var labels = new Array();
+    let labels = new Array();
     labels = []
-    for(var i = 0; i <= parameter; i += step){
-        labels.push(i);
+    let i = 0;
+
+    for(i; i <= parameter; i += step){
+        labels.push(i.toFixed(2));
     }
     return labels;
 }
+
 function chooseChart(){
-    console.log("in chooseChart func")
-    var rad = document.getElementsByName('criteria')
-    if(rad[0] == fun1()){
-        console.log("Before drawing chart")
-        let a = document.getElementById('a_value').value
-        let n = document.getElementById('n_value').value
-        let c_a = 0
-        let c_t = 0
+    let rad = document.getElementsByName('criteria')
+    var a = document.getElementById('a_value').value
+    var n = document.getElementById('n_value').value
+    var c_a = document.getElementById('ca_value').value
+    var c_t = document.getElementById('ct_value').value
+    if(rad[0] === getCheckedRadiobutton()){
         let h = 0.1
-        console.log("a parameter: " + a)
-        drawChart(getLabelsSet(a, h), getDataSet(a, n, c_a, c_t, h, a))
+        console.log(getDataSet(a, n, 0, 0, h, a))
+        drawChart(getLabelsSet(a, h), getDataSet(a, n, 0, 0, h, a))
     }
-    else if(rad[1] == fun1()){
-        console.log("Before drawing chart")
-        let a = document.getElementById('a_value').value
-        let n = document.getElementById('n_value').value
-        let c_a = 0
-        let c_t = 0
+    else if(rad[1] === getCheckedRadiobutton()){
         let h = 1
-        console.log("n parameter: " + n)
-        drawChart(getLabelsSet(n, h), getDataSet(a, n, c_a, c_t, h, n))
+        console.log("data set: ")
+        console.log(getDataSet(a, n, 0, 0, h, n))
+        drawChart(getLabelsSet(n, h), getDataSet(a, n, 0, 0, h, n))
     }
-    else if(rad[2] == fun1()){
-        console.log("Before drawing chart")
-        let a = document.getElementById('a_value').value
-        let n = document.getElementById('n_value').value
-        let c_a = document.getElementById('ca_value').value
-        let c_t = document.getElementById('ct_value').value
+    else if(rad[2] === getCheckedRadiobutton()){
         let h = 0.0001
-        console.log("ca parameter: " + c_a)
         drawChart(getLabelsSet(c_a, h), getDataSet(a, n, c_a, c_t, h, c_a))
     }
-    else if(rad[3] == fun1()){
-        console.log("Before drawing chart")
-        let a = document.getElementById('a_value').value
-        let n = document.getElementById('n_value').value
-        let c_a = document.getElementById('ca_value').value
-        let c_t = document.getElementById('ct_value').value
+    else if(rad[3] === getCheckedRadiobutton()){        
         let h = 100
-        console.log("a parameter: " + c_t)
+        
         drawChart(getLabelsSet(c_t, h), getDataSet(a, n, c_a, c_t, h, c_t))
     }
     else{
         console.log("Error! Try to choose correct variant of radiobutton")
     }
 }
-chooseChart()
-
